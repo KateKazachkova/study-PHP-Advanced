86e677a3f0bf5ef6d0088df4fc58f92007597d87
Kateryna Kazachkova <50364918+KateKazachkova@users.noreply.github.com>
2020-01-26 16:10:32 +0200

    Update hm2.php

--- hm2.php
+++ hm2.php
@@ -60,14 +60,78 @@ B переменной $month лежит число из интервала от
             break;
         case ($month == 12 || $month == 1 || $month == 2):
             echo 'Это зима';
+            break;
     }
 /*тернарка*/
     $month = rand(1, 12);
     echo 'Сейчас ' . $month . ' месяц';
     echo '<br>';
-    echo $month >=3 && $month < 6 ? 'Это весна' : ($month >= 6 && $month < 9 ? 
-        'Это лето' : ($month >=9 && $month < 12 ? 'Это осень' : 'Это зима'))
+    echo $month >=3 && $month < 6 ? 'Это весна' : 
+        ($month >= 6 && $month < 9 ? 'Это лето' : 
+        ($month >=9 && $month < 12 ? 'Это осень' : 'Это зима'));
 
+
+/* Уровень 1. Задача 3.
+Определение интервала времени. В переменной $minutes – лежит случайное число от 1 до 59 (rand(1, 59)). 
+Определить к какой четверти относится сейчас минуты.*/
+/*if else*/
+    $minutes = rand(1, 59);
+    echo 'Сейчас ' . $minutes . ' минута';
+    echo '<br>';
+    if ($minutes >=1 && $minutes < 15)
+        echo 'Это первая четверть';
+    elseif ($minutes >= 16 && $minutes < 30)
+        echo 'Это вторая четверть';
+    elseif ($minutes >=30 && $minutes < 45)
+        echo 'Это третья четверть';
+    else echo 'Это четвертая четверть';
+/*switch-case*/
+    $minutes = rand(1, 59);
+    echo 'Сейчас ' . $minutes . ' минута';
+    echo '<br>';
+    switch ($minutes) {
+        case $minutes >=1 && $minutes < 15 :
+             echo 'Это первая четверть';
+             break;
+        case $minutes >= 16 && $minutes < 30 :
+             echo 'Это вторая четверть';
+             break;
+        case $minutes >=30 && $minutes < 45 :
+             echo 'Это третья четверть';
+             break;
+        case $minutes >=45 && $minutes < 60 :
+             echo 'Это четвертая четверть';
+             break;
+    }
+/*тернарка*/
+    $minutes = rand(1, 59);
+    echo 'Сейчас ' . $minutes . ' минута';
+    echo '<br>';
+    echo $minutes >=1 && $minutes < 15 ? 'Это первая четверть':
+         ($minutes >= 16 && $minutes < 30 ? 'Это вторая четверть' :
+         ($minutes >=30 && $minutes < 45 ? 'Это третья четверть' : 'Это четвертая четверть'));
+
+
+/* Уровень 1. Задача 4.
+Если переменная $a = 0 или $a = 2, то прибавьте к ней 7, иначе поделите ее на 10. Выведите результат на экран. 
+Проверьте работу программы при $a, равном 5, 0, -3, 2..*/
+/*if else*/
+    $a = 2;
+    if ($a == 0 || $a == 2) echo ($a+7);
+       else echo ($a / 10);
+/*switch-case*/
+    $a = 2;
+    switch ($a) {
+        case ($a == 0 || $a == 2) :
+          echo $a = $a+7 ;
+          break;
+        case ($a !== 0 || $a !== 2) :
+          echo $a = $a / 10 ;
+          break;
+    }
+/*тернарка*/
+    $a = 2;
+    echo $a == 0 || $a == 2 ? $a = $a+7 : $a = $a / 10;
 
 
 
